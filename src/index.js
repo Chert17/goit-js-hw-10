ed77ee661a9af0e18d845b41ef318a753447a55f
Andrii Chertkov <andriichertkov@gmail.com>
2022-11-21 18:33:13 +0200

    updata-1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,94 @@
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  inputEl: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.inputEl.addEventListener(
+  'input',
+  debounce(onSearchCountry, DEBOUNCE_DELAY)
+);
+
+function onSearchCountry(e) {
+  const inputValue = e.target.value.trim();
+  if (!inputValue) {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+  }
+  fetchCountries(inputValue)
+    .then(countries => {
+      console.log(countries);
+
+      if (countries.length > 10) {
+        refs.countryList.innerHTML = '';
+        refs.countryInfo.innerHTML = '';
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.',
+          {
+            timeout: 1000,
+            showOnlyTheLastOne: true,
+          }
+        );
+      }
+
+      if (countries.length >= 2 && countries.length <= 10) {
+        refs.countryList.innerHTML = renderCountryList(countries);
+        refs.countryInfo.innerHTML = '';
+      }
+
+      if (countries.length === 1) {
+        refs.countryInfo.innerHTML = renderCountryInfo(countries);
+        refs.countryList.innerHTML = '';
+      }
+    })
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name', {
+        timeout: 1000,
+        showOnlyTheLastOne: true,
+      });
+    });
+}
+
+function renderCountryList(countries) {
+  return countries
+    .map(({ name, flags }) => {
+      return `<li class="country-list__item">
+  <img src="${flags.svg}" alt="${name.official}" width="30" height="30">
+  <p class="countru-list__name">${name.official}</p>
+</li>`;
+    })
+    .join('');
+}
+
+function renderCountryInfo(countries) {
+  return countries
+    .map(({ name, flags, capital, population, languages }) => {
+      return `<h2 class="title-info">
+  <img
+    src="${flags.svg}"
+    alt="${name.official}"
+    width="30"
+    height="30"
+  />${name.official}
+</h2>
+<ul class="list-info">
+  <li class="list-info__item">
+    <p><strong>Capital:</strong> ${capital}</p>
+  </li>
+  <li>
+    <p><strong>Population:</strong> ${population}</p>
+  </li>
+  <li>
+    <p><strong>Languages:</strong> ${Object.values(languages)}</p>
+  </li>
+</ul>`;
+    })
+    .join('');
+}
